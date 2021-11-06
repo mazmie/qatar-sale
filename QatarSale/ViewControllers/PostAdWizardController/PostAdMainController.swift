42c345c1d51dccb1afdffa3e5001c0a0c4963e34
Azmi al-Hashimi <azmi@arroot.net>
2021-11-06 17:51:33 +0300

    integrated PostAdMainController into the app

--- QatarSale/ViewControllers/PostAdWizardController/PostAdMainController.swift
+++ QatarSale/ViewControllers/PostAdWizardController/PostAdMainController.swift
@@ -12,19 +12,39 @@ class PostAdMainController: UIViewController {
     @IBOutlet weak var titleLbl: UILabel!
     @IBOutlet weak var categoryLbl: UILabel!
     @IBOutlet weak var selectedCategoryLbl: UILabel!
+    @IBOutlet weak var pagerContainerView: UIView!
+    
+    let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    var direction: UIPageViewController.NavigationDirection = .forward
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setup()
     }
-
+    
     func setup() {
         titleLbl.setup(font: .nunito(type: .regular, size: 20), color: ._282828)
-        
         categoryLbl.setup(font: .nunito(type: .bold, size: 18), color: ._282828)
-        
         selectedCategoryLbl.setup(font: .nunito(type: .regular, size: 15), color: ._878787)
+        
+        setupPager()
+        setCurrentController()
     }
-
+    
+    private func setupPager() {
+        pagerContainerView.addSubview(pageViewController.view)
+        self.addChild(pageViewController)
+        pageViewController.didMove(toParent: self)
+    }
+    
+    private func setCurrentController() {
+        DispatchQueue.main.async {
+            self.pageViewController.setViewControllers(
+                [PostAdCategoryController()],
+                direction: self.direction,
+                animated: true,
+                completion: nil)
+        }
+    }
 }
