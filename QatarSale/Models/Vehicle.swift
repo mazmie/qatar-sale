e476b53e2a1e995d4ac5129bf29309ec7524cb11
Azmi al-Hashimi <azmi@arroot.net>
2021-11-06 16:31:32 +0300

    added VehicleRepo

---
+++ QatarSale/Models/Vehicle.swift
@@ -0,0 +1,14 @@
+//
+//  Vehicle.swift
+//  QatarSale
+//
+//  Created by Azmi on 6.11.2021.
+//
+
+import Foundation
+import UIKit
+
+struct Vehicle {
+    var title: String
+    var image: UIImage
+}
---
+++ QatarSale/Repositories/VehiclesRepo.swift
@@ -0,0 +1,20 @@
+//
+//  VehiclesRepo.swift
+//  QatarSale
+//
+//  Created by Azmi on 6.11.2021.
+//
+
+import UIKit
+
+class VehiclesRepo {
+    
+    static let items: [Vehicle] = [
+        Vehicle(title: "Phone Cal Tablets", image: UIImage(named: "vehicle0")!),
+        Vehicle(title: "Phone Cal Tablets", image: UIImage(named: "vehicle1")!),
+        Vehicle(title: "Phone Cal Tablets", image: UIImage(named: "vehicle2")!),
+        Vehicle(title: "Phone Cal Tablets", image: UIImage(named: "vehicle3")!),
+        Vehicle(title: "Phone Cal Tablets", image: UIImage(named: "vehicle4")!),
+        Vehicle(title: "Phone Cal Tablets", image: UIImage(named: "vehicle0")!)
+    ]
+}
