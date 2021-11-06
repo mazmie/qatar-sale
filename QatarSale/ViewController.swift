9dc9356ddf7b5e09b803c69270e348e15989c969
Azmi al-Hashimi <azmi@arroot.net>
2021-11-06 12:20:26 +0300

    Initial Commit

---
+++ QatarSale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QatarSale
+//
+//  Created by Azmi on 6.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
